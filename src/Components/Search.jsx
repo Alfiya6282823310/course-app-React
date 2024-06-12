df2bb2183f737be1f7f0ac26715ad476151364f1
Alfiya6282823310 <knalfiya@gmail.com>
2024-06-12 11:34:10 +0530

    search api completed

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,27 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 const Search = () => {
-  return (
-    <div>
-        <Navbar/>
-        <div className="container">
-            <div className="row">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row g-3">
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label htmlFor="" className="form-label">Course Title</label>
-                            <input type="text" className="form-label" />
+    const [data, changeData] = useState(
+        {
+            "title": ""
+        }
+    )
+    const [result,setResult] = useState([])
+    const inputHandler = (event) => {
+        changeData({ ...data, [event.target.name]: event.target.value })
+    }
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8084/search",data).then(
+            (response)=>{
+            setResult(response.data)
+            }
+        ).catch().finally()
+    }
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Course Title</label>
+                                <input type="text" className="form-label" name='title' value={data.title} onChange={inputHandler} />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-info" onClick={readValue}>Search</button>
+                            </div>
                         </div>
-                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-info">Search</button>
-                        </div>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">description</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Duration</th>
+                                    <th scope="col">Venue</th>
+                                    <th scope="col">trainer Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    result.map(
+                                        (value, index) => {
+                                            return <tr>
+                                                <td>{value.title }</td>
+                                                <td>{ value.desc}</td>
+                                                <td>{ value.date}</td>
+                                                <td>{value.duration }</td>
+                                                <td>{value.venue }</td>
+                                                <td>{value.trainername }</td>
+                                            </tr>
+                                        }
+                                    )
+                                }
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Search
